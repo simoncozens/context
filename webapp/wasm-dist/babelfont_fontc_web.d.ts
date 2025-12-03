4a8fbb23cfa50de42f6229759888f13edfdc6aaa
Simon Cozens <simon@simon-cozens.org>
2025-12-03 14:01:43 +0000

    Recompile wasm

--- webapp/wasm-dist/babelfont_fontc_web.d.ts
+++ webapp/wasm-dist/babelfont_fontc_web.d.ts
@@ -1,23 +1,34 @@
 /* tslint:disable */
 /* eslint-disable */
-export function init(): void;
+
 /**
  * Compile a font from babelfont JSON directly to TTF
- * 
+ *
  * This is the main entry point that takes a .babelfont JSON string
  * and produces compiled TTF bytes.
- * 
+ *
  * # Arguments
  * * `babelfont_json` - JSON string in .babelfont format
- * 
+ * * `options` - Compilation options:
+ *  - `skip_kerning`: bool - Skip creation of kern tables
+ *  - `skip_features`: bool - Skip OpenType feature compilation
+ *  - `skip_metrics`: bool - Skip metrics compilation
+ *  - `skip_outlines`: bool - Skip `glyf`/`gvar` table creation
+ *  - `dont_use_production_names`: bool - Don't use production names for glyphs
+ *  - `subset_glyphs`: String[] - List of glyph names to include
+ *
  * # Returns
  * * `Vec<u8>` - Compiled TTF font bytes
  */
-export function compile_babelfont(babelfont_json: string): Uint8Array;
+export function compile_babelfont(babelfont_json: string, options: any): Uint8Array;
+
 /**
  * Legacy function for compatibility
  */
 export function compile_glyphs(_glyphs_json: string): Uint8Array;
+
+export function init(): void;
+
 /**
  * Get version information
  */
@@ -27,19 +38,22 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly compile_babelfont: (a: number, b: number) => [number, number, number, number];
+  readonly compile_babelfont: (a: number, b: number, c: any) => [number, number, number, number];
   readonly compile_glyphs: (a: number, b: number) => [number, number, number, number];
   readonly init: () => void;
   readonly version: () => [number, number];
-  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
   readonly __wbindgen_externrefs: WebAssembly.Table;
   readonly __externref_table_dealloc: (a: number) => void;
   readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
+
 /**
 * Instantiates the given `module`, which can either be bytes or
 * a precompiled `WebAssembly.Module`.
